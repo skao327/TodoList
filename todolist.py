241a50b053a1edeea5c6f1742f32b8bae848c06d
jiyoung <park050505@@gmail.com>
2025-05-20 21:52:18 +0900

    예외 처리 추가

--- todolist.py
+++ todolist.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import mysql.connector
 from tkinter import messagebox, simpledialog
 from datetime import datetime, date
+import traceback
 
 class Todo:
 
@@ -20,7 +21,15 @@ class Todo:
             password="mypass123",
             database="todo_db"
         )
-        self.conn = mysql.connector.connect(**self.dbinfo)
+        try:
+            self.conn = mysql.connector.connect(use_pure=True, **self.dbinfo)
+        except Exception as err:
+            print("❌ MySQL 연결 중 예외 발생:")
+            import traceback
+            traceback.print_exc()
+            from tkinter import messagebox
+            messagebox.showerror("DB 연결 실패", str(err))
+            raise
         self.cursor = self.conn.cursor()
         self.cursor.execute("SELECT DATABASE();")
         print(self.cursor.fetchone())
